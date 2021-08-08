5c86457b5770fa5c12ba896ecabdce4690789008
Alex Marshall <alex.k.marshall83@gmail.com>
2021-08-08 18:41:52 +0100

    move db setup/teardown out of global setup

--- client/features/invoice/model.test.ts
+++ client/features/invoice/model.test.ts
@@ -1,4 +1,17 @@
+import { execSync } from 'child_process'
+import prisma from '../../prisma'
 import { getInvoiceSummaries } from './model'
+
+beforeEach(() => {
+  const dbUrl = `file:./test-${process.env.JEST_WORKER_ID}.db`
+  process.env.DATABASE_URL = dbUrl
+
+  const command = `DATABASE_URL=${dbUrl} npx prisma migrate deploy`
+  execSync(command)
+})
+afterEach(async () => {
+  await prisma.$disconnect()
+})
 
 it('should return empty array if no invoices', async () => {
   const invoiceSummaries = await getInvoiceSummaries()
--- client/jest.setup.node.js
+++ client/jest.setup.node.js
@@ -1,16 +1,3 @@
-import { execSync } from 'child_process'
-import prisma from './prisma'
-
 process.env.DEBUG_PRINT_LIMIT = 1500
 
-beforeEach(() => {
-  const dbUrl = `file:./test-${process.env.JEST_WORKER_ID}.db`
-  process.env.DATABASE_URL = dbUrl
-
-  const command = `DATABASE_URL=${dbUrl} npx prisma migrate deploy`
-  execSync(command)
-})
-afterAll(() => {})
-afterEach(async () => {
-  await prisma.$disconnect()
-})
+export {}
