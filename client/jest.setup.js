53e093b4fa97009e35cc0582539880a7a2986a59
Alex Marshall <alex.k.marshall83@gmail.com>
2021-08-08 17:13:27 +0100

    add database deploy to tests

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,11 +1,19 @@
 import '@testing-library/jest-dom'
 import 'whatwg-fetch'
+import { execSync } from 'child_process'
 import { server } from '@mocks/server/test-server'
 
 process.env.DEBUG_PRINT_LIMIT = 1500
 
 beforeAll(() => {
   server.listen()
+})
+beforeEach(() => {
+  const dbUrl = `file:./test-${process.env.JEST_WORKER_ID}.db`
+  process.env.DATABASE_URL = dbUrl
+
+  const command = `DATABASE_URL=${dbUrl} npx prisma migrate deploy`
+  execSync(command)
 })
 afterAll(() => {
   server.close()
